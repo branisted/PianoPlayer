bad321dd56806bca99d30632505e1b055388f136
Dragoș <branisted@gmail.com>
2025-06-21 20:51:58 +0300

    Created basic project structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PianoPlayer
+
+python3 -m venv .venv
+source .venv/bin/activate // Or IDE Alternative
+pip install -r requirements.txt
---
+++ piano/constants.py
@@ -0,0 +1,10 @@
+DELTARUNE_NOTES = [
+
+]
+# notes from the game - need to add
+
+NOTE_TO_KEY = {
+    "C4": "a", 
+    "D4": "s" 
+}
+# key bindings for the auto-player
